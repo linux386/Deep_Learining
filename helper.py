8b4f6ceae7d24838ddd3ec52e3e2688d3c166f9d
kkang <linux386@hanmail.net>
2019-01-03 16:41:47 +0900

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,12 +1,18 @@
 
 import numpy as np
+import pandas as pd
 
 # build datasets
-def build_dataset(data):
-  for i in range(0, len(time_series) - seq_length):
-    _x = time_series[i:i + seq_length, :]
-    _y = time_series[i + seq_length, [-1]]  # Next close price
-    print(_x, "->", _y)
-    dataX.append(_x)
-    dataY.append(_y)
-    return np.array(dataX), np.array(dataY)
+def load_data(data, seq):
+    dataX = []
+    dataY = []
+    for  i in range(num-seq):
+        x = xy[i:seq+i]
+        y = xy[i+seq, -1]
+        lastX  = xy[-seq:]
+        dataX.append(x)
+        dataY.append(y)
+    dataX = np.array(dataX)
+    dataY = np.array(dataY)
+    return dataX, dataY, lastX 
+
